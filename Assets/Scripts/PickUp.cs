cd762bf3e84b8b93a9c7141f2841d22dd3fc387f
budi <susucoklat341@gmail.com>
2022-12-04 05:15:23 +0700

    fix bug (detect closest object)

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -7,58 +7,67 @@ public class PickUp : MonoBehaviour
     public GameObject poin;
     private GameObject[] kotak;
     private GameObject main;
-
+    float[] jarak;
+    public GameObject[] tongSampah;
     // Start is called before the first frame update
     void Start()
     {
         kotak = GameObject.FindGameObjectsWithTag("halangan");
+        tongSampah = GameObject.FindGameObjectsWithTag("tong sampah");
+        jarak = new float[kotak.Length];
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < kotak.Length; i++)
+        for (int i = 0; i < jarak.Length; i++)
         {
-            if (Vector3.Distance(this.transform.position, kotak[i].transform.position) <= 2f)
+            jarak[i] = Vector3.Distance(transform.position, kotak[i].transform.position);
+            if (main == null)
             {
-                if (Input.GetKeyDown(KeyCode.E))
+                if (jarak[i]>0&&jarak[i]<0.5)
                 {
-                    if (main == null)
+                    if (Input.GetKeyDown(KeyCode.E))
                     {
-                        
-                        kotak[i].transform.SetParent(poin.transform);
                         main = kotak[i];
+                        kotak[i].transform.SetParent(poin.transform);
                         kotak[i].transform.localPosition = Vector3.zero;
                     }
-                    else
-                    {
-                        return;
-                    }
                 }
-                else if (Input.GetKeyDown(KeyCode.G))
+            }
+        }
+
+        for (int i = 0; i < tongSampah.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                if (Vector3.Distance(transform.position, tongSampah[i].transform.position) <= 2)
+                {
+                    if (main != null)
+                    {
+                        //Destroy(poin.transform.GetChild(0).gameObject);
+                        main = null;
+                    }
+
+                }
+                else
                 {
                     if (main != null)
                     {
                         poin.transform.DetachChildren();
                         main = null;
                     }
-                    else
-                    {
-                        return;
-                    }
+
                 }
+
             }
+
+
         }
     }
 
-
-    void pickUp()
+    void checknearest()
     {
         
-    }
-
-    void Drop()
-    {
-       
     }
 }
