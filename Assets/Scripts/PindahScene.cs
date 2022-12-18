5f265df10d451896c84977987831199642228d3d
Putra Ramadhan <putraramadhan2705@gmail.com>
2022-12-18 13:36:15 +0700

    Nambah animasi

---
+++ Assets/Scripts/PindahScene.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PindahScene : MonoBehaviour
+{
+   public void MulaiGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+   public void KeluarGame()
+    {
+        Application.Quit();
+        Debug.Log("Keluar");
+    }
+}
